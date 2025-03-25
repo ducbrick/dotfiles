7b243649800498b034b06780ebf96336749487eb
ducbrick <ducbrick@gmail.com>
2025-03-25 22:54:48 +0700

    switch terminal

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -37,7 +37,7 @@ from libqtile.utils import guess_terminal
 ####################################################################################################
 
 mod = "mod4"
-terminal = "kitty"
+terminal = "alacritty"
 
 keys = [
     # A list of available commands that can be bound to keys can be found
