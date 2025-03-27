b444e307bc2db522c22da14be3394e754a490f3f
ducbrick <ducbrick@gmail.com>
2025-03-27 22:22:32 +0700

    update tide config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    set --universal tide_left_prompt_items pwd newline character
+    set --universal tide_pwd_bg_color brmagenta
+    set --universal tide_pwd_color_anchors black
+    set --universal tide_pwd_color_dirs black
+    set --universal tide_pwd_color_truncated_dirs brblack
+    set --universal tide_time_bg_color green
 end
