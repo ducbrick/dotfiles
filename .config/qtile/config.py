d0ae9e80a0dc60887901f12beb6f8d7446d834cd
ducbrick <ducbrick@gmail.com>
2025-04-12 19:37:16 +0700

    update

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -375,7 +375,7 @@ screens = [
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
-        wallpaper='~/.config/qtile/wallpaper.png',
+        wallpaper='~/.config/qtile/wallpaper 2.png',
         # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
         # By default we handle these events delayed to already improve performance, however your system might still be struggling
         # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
