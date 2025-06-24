c6c4cf319444443d4ada3d12954d396c5fb5494b
ducbrick <ducbrick@gmail.com>
2025-06-25 00:18:53 +0700

    upd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,13 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+	# Commands to run in interactive sessions can go here
 
-    set --universal fish_greeting
-    set --universal tide_left_prompt_items pwd newline character
-    set --universal tide_pwd_bg_color brmagenta
-    set --universal tide_pwd_color_anchors black
-    set --universal tide_pwd_color_dirs black
-    set --universal tide_pwd_color_truncated_dirs brblack
-    set --universal tide_time_bg_color green
+	set --universal fish_greeting
+	set --universal tide_left_prompt_items pwd newline character
+	set --universal tide_pwd_bg_color brmagenta
+	set --universal tide_pwd_color_anchors black
+	set --universal tide_pwd_color_dirs black
+	set --universal tide_pwd_color_truncated_dirs brblack
+	set --universal tide_time_bg_color green
+
+	set -gx EDITOR nvim
 end
