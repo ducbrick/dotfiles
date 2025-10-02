90b376f6efae9ddb5d8ee88a565c9644b3dc07d1
ducbrick <ducbrick@gmail.com>
2025-10-02 23:17:20 +0700

    upd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,12 @@
 if status is-interactive
 	# Commands to run in interactive sessions can go here
 
-	set --universal fish_greeting
-	set --universal tide_left_prompt_items pwd newline character
-	set --universal tide_pwd_bg_color brmagenta
-	set --universal tide_pwd_color_anchors black
-	set --universal tide_pwd_color_dirs black
-	set --universal tide_pwd_color_truncated_dirs brblack
-	set --universal tide_time_bg_color green
+	set --global fish_greeting
+	set --global tide_left_prompt_items pwd git newline character
+	set --global tide_pwd_bg_color brmagenta
+	set --global tide_pwd_color_anchors black
+	set --global tide_pwd_color_dirs black
+	set --global tide_pwd_color_truncated_dirs brblack
 
 	fish_add_path /home/ducbrick/.local/bin
 
